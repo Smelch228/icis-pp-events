8ef32f57b8f9a590463623e6fabc738930d28b3c
Smelch228 <salamandr258@gmail.com>
2021-11-02 23:20:48 +0300

    initial, prepare for event project

---
+++ pages/events/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Event(){
+  return(
+      <div>
+          <h1>this is event with some id, bitch</h1>
+          <p>eeee</p>
+      </div>
+  )
+}
---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function FirstPost(){
+    return <h1>Idi nahuy</h1>
+}
