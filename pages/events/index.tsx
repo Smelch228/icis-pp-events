261575578ca9da253333d4537b43f61994cfc4ae
fedotarte <artyom.fedotov93@gmail.com>
2022-04-13 00:05:26 +0300

    add fetched data

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -4,6 +4,26 @@ import AddEventButton from "../../components/buttons/CreateEventButton";
 import EventFilterButton from "../../components/buttons/EventFilterButtton";
 import { css } from "@emotion/css";
 import Typography from '@mui/material/Typography'
+import SearchOffIcon from '@mui/icons-material/SearchOff';
+
+interface IEvent
+{
+  id: string;
+  eventName: string,
+  content: string,
+  eventFormat: boolean,
+  startTime: string,
+  endTime: string,
+  location: string,
+  published: string,
+  orginezer: number,
+  category: Array<number>
+}
+
+interface IEventsProps {
+  data?: IEvent[]
+}
+
 
 const parentStyles = css`
       margin: 0 auto;
@@ -15,7 +35,14 @@ const parentStyles = css`
       min-width: 411px;
       width: 411px;`
 
-const Events = () => (
+const Events = ({ data } : IEventsProps) => {
+
+  console.log({data});
+  
+
+  const events = data ?? [...Array(26).fill('events')];
+
+  return (
   <div className={parentStyles}>
     <div
       className={css`
@@ -31,20 +58,29 @@ const Events = () => (
           justify-content: center;
         `}
       >
-        <AddEventButton /> <EventFilterButton />
+        <AddEventButton /> 
+        <EventFilterButton />
       </div>
     </div>
-    
-      <ul
-        className={css`
-          padding-inline-start: 0;
-        `}
-      >
-        {[...Array(26).fill('events')].map((card, i) => (
-          <Card key={i}/>
-        ))}
-      </ul>
+          {true && ( <ul
+            className={css`
+              padding-inline-start: 0;
+            `}
+          >
+            {events.map((event: object, i: number) => (
+              <Card event={event} key={i}/>
+            ))}
+          </ul>)}
   </div>
-)
+  )
+}
 
 export default Events;
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:8000/api/events`)
+  const data = await res.json()
+  // Pass data to the page via props
+  return { props: { data } }
+}
